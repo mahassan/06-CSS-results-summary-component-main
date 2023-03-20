a7892da03669e3fb00c5a05ba817eb823e97e24a
Ammar Hassan <ammar@ammarhassan.co.uk>
2023-03-20 09:11:18 +0000

    latest

--- app.js
+++ app.js
@@ -1,14 +1,43 @@
-import {LitElement, html, unsafeCSS} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js';
-
+import {LitElement, html, css} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js';
 class Results extends LitElement{
-    // static styles = css`
-    //  ${unsafeCSS()}
-    // `
+    static styles = css`
+  @import url('https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@500;700;800&family=Overpass:wght@400;700&display=swap');
+     div.container{
+        //display: flex;
+        text-align: center;
+     }
+     header{
+        height: 353px;
+        padding-top:26px;
+        background: linear-gradient( hsl(252, 100%, 67%), hsl(241, 81%, 54%));
+        border-bottom-left-radius:40px;
+        border-bottom-right-radius:40px;
+
+     }
+     div.circular{
+        height: 134px;
+        width: 143px;
+        background: linear-gradient(hsla(256, 72%, 46%, 1), hsla(241, 72%, 46%, 0));
+        border-radius: 50%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+     }
+     div.circular>h1{
+        color:white;
+     }
+    `
     render(){
         const header = html`
-            <header>
-                <strong><p>Your Results</p></strong>
-            </header>
+            <div class='container'>
+                <header>
+                    <strong><p>Your Results</p></strong>
+                    <div class='circular'>
+                        <h1>76</h1>
+                        of 100
+                    </div>
+                </header>
+            </div>
         `
         return html`
             ${header}
